05238fbd523d8af4e1794c1bd7ba9d05352794c2
Rodger H Elliott, Jr <rodgere1@comcast.net>
2023-05-02 07:37:11 -0400

    dfsdfgsd

--- main-bookmarks_to_readme.py
+++ main-bookmarks_to_readme.py
@@ -7,7 +7,13 @@ from typing import List
 from tqdm import tqdm
 
 def fetch_url_content(url: str) -> str:
-    response = requests.get(url)
+    """Fetches the content of the given URL."""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(f"Request failed with exception: {e}")
+    
     if response.status_code == 200:
         return response.text
     elif response.status_code in (403, 404, 410, 429, 522):
@@ -68,32 +74,40 @@ def extract_urls_from_html(file_path: str) -> List[str]:
 
     return urls
 
-
-
 def create_readme_md(urls: List[str], output_dir: str):
     h2t = html2text.HTML2Text()
     h2t.ignore_links = True
     h2t.ignore_images = True
     error_list = []
 
+    entries = []
+
+    for url in tqdm(urls, desc="Processing URLs"):
+        try:
+            content = fetch_url_content(url)
+            metadata = extract_metadata(content)
+            favicon_url = extract_favicon_url(content)
+
+            entry = ""
+
+            if favicon_url and check_favicon_url(favicon_url):
+                entry += f"![Favicon]({favicon_url}) "
+
+            entry += f"[{metadata.get('title', url)}]({url})\n\n"
+
+            if "description" in metadata:
+                entry += f"{h2t.handle(metadata['description'])}\n\n"
+
+            entries.append((metadata.get('title', url), entry))
+
+        except requests.exceptions.RequestException as e:
+            error_list.append(f"Error processing {url}: {e}")
+
+    entries.sort()
+
     with open(os.path.join(output_dir, "README.md"), "w", encoding="utf-8") as f:
-        for url in tqdm(urls, desc="Processing URLs"):
-            try:
-                content = fetch_url_content(url)
-                metadata = extract_metadata(content)
-                favicon_url = extract_favicon_url(content)
-
-                if favicon_url and check_favicon_url(favicon_url):
-                    f.write(f"![Favicon]({favicon_url}) ")
-
-                f.write(f"[{metadata.get('title', url)}]({url})\n\n")
-
-                if "description" in metadata:
-                    f.write(f"{h2t.handle(metadata['description'])}\n\n")
-
-            except requests.exceptions.RequestException as e:
-                error_list.append(f"Error processing {url}: {e}")
-
+        for _, entry in entries:
+            f.write(entry)
             f.write("---\n\n")
 
         # Write errors at the end of the file
@@ -101,10 +115,6 @@ def create_readme_md(urls: List[str], output_dir: str):
             f.write("## Errors\n\n")
             for error in error_list:
                 f.write(f"- {error}\n")
-
-
-
-
 def main():
     parser = argparse.ArgumentParser(
         description="Process HTML bookmarks file and generate README.md with metadata."
